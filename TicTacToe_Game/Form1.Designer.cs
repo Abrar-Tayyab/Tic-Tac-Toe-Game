60b3e26d7b215cf67310674b626dc4776343c7b0
Abrar-Tayyab <abrar.tayyab.3@gmail.com>
2020-02-11 18:15:35 +0500

    Tic Tac Toe Game using C#

---
+++ TicTacToe_Game/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace TicTacToe_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A00 = new System.Windows.Forms.Button();
+            this.A01 = new System.Windows.Forms.Button();
+            this.A02 = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.NGB = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.XWin = new System.Windows.Forms.Label();
+            this.Draws = new System.Windows.Forms.Label();
+            this.OWin = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // A00
+            // 
+            this.A00.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A00.Location = new System.Drawing.Point(14, 110);
+            this.A00.Name = "A00";
+            this.A00.Size = new System.Drawing.Size(83, 77);
+            this.A00.TabIndex = 0;
+            this.A00.UseVisualStyleBackColor = true;
+            this.A00.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A01
+            // 
+            this.A01.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A01.Location = new System.Drawing.Point(101, 110);
+            this.A01.Name = "A01";
+            this.A01.Size = new System.Drawing.Size(83, 77);
+            this.A01.TabIndex = 1;
+            this.A01.UseVisualStyleBackColor = true;
+            this.A01.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A02
+            // 
+            this.A02.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A02.Location = new System.Drawing.Point(188, 110);
+            this.A02.Name = "A02";
+            this.A02.Size = new System.Drawing.Size(83, 77);
+            this.A02.TabIndex = 2;
+            this.A02.UseVisualStyleBackColor = true;
+            this.A02.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A22
+            // 
+            this.A22.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A22.Location = new System.Drawing.Point(188, 272);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(83, 77);
+            this.A22.TabIndex = 5;
+            this.A22.UseVisualStyleBackColor = true;
+            this.A22.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A21
+            // 
+            this.A21.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A21.Location = new System.Drawing.Point(101, 272);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(83, 77);
+            this.A21.TabIndex = 4;
+            this.A21.UseVisualStyleBackColor = true;
+            this.A21.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A20
+            // 
+            this.A20.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A20.Location = new System.Drawing.Point(14, 272);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(83, 77);
+            this.A20.TabIndex = 3;
+            this.A20.UseVisualStyleBackColor = true;
+            this.A20.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A10
+            // 
+            this.A10.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A10.Location = new System.Drawing.Point(14, 191);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(83, 77);
+            this.A10.TabIndex = 3;
+            this.A10.UseVisualStyleBackColor = true;
+            this.A10.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A11
+            // 
+            this.A11.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A11.Location = new System.Drawing.Point(101, 191);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(83, 77);
+            this.A11.TabIndex = 4;
+            this.A11.UseVisualStyleBackColor = true;
+            this.A11.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A12
+            // 
+            this.A12.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A12.Location = new System.Drawing.Point(188, 191);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(83, 77);
+            this.A12.TabIndex = 5;
+            this.A12.UseVisualStyleBackColor = true;
+            this.A12.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // NGB
+            // 
+            this.NGB.Font = new System.Drawing.Font("Monotype Corsiva", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NGB.ForeColor = System.Drawing.Color.SteelBlue;
+            this.NGB.Location = new System.Drawing.Point(14, 375);
+            this.NGB.Name = "NGB";
+            this.NGB.Size = new System.Drawing.Size(86, 35);
+            this.NGB.TabIndex = 6;
+            this.NGB.Text = "New Game";
+            this.NGB.UseVisualStyleBackColor = true;
+            this.NGB.Click += new System.EventHandler(this.NGB_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(101, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 35);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Monotype Corsiva", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(338, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // XWin
+            // 
+            this.XWin.AutoSize = true;
+            this.XWin.BackColor = System.Drawing.Color.Teal;
+            this.XWin.Font = new System.Drawing.Font("Maiandra GD", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XWin.ForeColor = System.Drawing.Color.Maroon;
+            this.XWin.Location = new System.Drawing.Point(333, 136);
+            this.XWin.Name = "XWin";
+            this.XWin.Size = new System.Drawing.Size(78, 29);
+            this.XWin.TabIndex = 9;
+            this.XWin.Text = "label1";
+            this.XWin.Click += new System.EventHandler(this.XWin_Click);
+            // 
+            // Draws
+            // 
+            this.Draws.AutoSize = true;
+            this.Draws.BackColor = System.Drawing.Color.Teal;
+            this.Draws.Font = new System.Drawing.Font("Maiandra GD", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Draws.ForeColor = System.Drawing.Color.Maroon;
+            this.Draws.Location = new System.Drawing.Point(333, 298);
+            this.Draws.Name = "Draws";
+            this.Draws.Size = new System.Drawing.Size(82, 29);
+            this.Draws.TabIndex = 10;
+            this.Draws.Text = "label3";
+            this.Draws.Click += new System.EventHandler(this.Draws_Click);
+            // 
+            // OWin
+            // 
+            this.OWin.AutoSize = true;
+            this.OWin.BackColor = System.Drawing.Color.Teal;
+            this.OWin.Font = new System.Drawing.Font("Maiandra GD", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OWin.ForeColor = System.Drawing.Color.Maroon;
+            this.OWin.Location = new System.Drawing.Point(333, 217);
+            this.OWin.Name = "OWin";
+            this.OWin.Size = new System.Drawing.Size(82, 29);
+            this.OWin.TabIndex = 10;
+            this.OWin.Text = "label2";
+            this.OWin.Click += new System.EventHandler(this.OWin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(30, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(385, 30);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "T I C  T A C  T O E  G A M E";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.label2.Font = new System.Drawing.Font("Maiandra GD", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Silver;
+            this.label2.Location = new System.Drawing.Point(102, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 29);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(449, 422);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OWin);
+            this.Controls.Add(this.Draws);
+            this.Controls.Add(this.XWin);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.NGB);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A02);
+            this.Controls.Add(this.A01);
+            this.Controls.Add(this.A00);
+            this.Name = "Form1";
+            this.Text = "TIC TAC TOE GAME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A00;
+        private System.Windows.Forms.Button A01;
+        private System.Windows.Forms.Button A02;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button NGB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label XWin;
+        private System.Windows.Forms.Label Draws;
+        private System.Windows.Forms.Label OWin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TicTacToe_Game/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int player = 2; // even= X turn; odd = O turn;
+        public int turns = 0; // counting turns;
+        //counting wins for both players and draws;
+        public int s1 = 0;
+        public int s2 = 0;
+        public int sd = 0; 
+        string title = "Winner Result"; 
+        
+
+        System.Media.SoundPlayer sound1 = new System.Media.SoundPlayer(@"C:\Users\Abrar Tayyab\Documents\Visual Studio 2013\Projects\TicTacToe_Game\wavfile1.wav");
+        System.Media.SoundPlayer sound2 = new System.Media.SoundPlayer(@"C:\Users\Abrar Tayyab\Documents\Visual Studio 2013\Projects\TicTacToe_Game\wavfile2.wav");                   
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XWin.Text = "X: " + s1;
+            OWin.Text = "O: " + s2;
+            Draws.Text = "Draws: " + sd;
+            label2.Text = "PLAY NOW";
+
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+                if(player%2==0)
+                {
+                    button.Text = "X";
+                    button.ForeColor = Color.SlateGray;
+                    sound1.Play();
+                    label2.Text = "O Turn Now";
+                    player++;
+                    turns++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    button.ForeColor = Color.SeaGreen;
+                    sound1.Play();
+                    label2.Text = "X Turn Now";
+                    player++;
+                    turns++;
+                }
+                if(CheckDraw()==true)
+                {
+                    MessageBox.Show("Tie Game!");
+                    sd++;
+                    AfterWinGame();
+                }
+                if(CheckWinner() == true)
+                {
+
+                    if(button.Text=="X")
+                    {
+                        MessageBox.Show("X Won!", title);
+                        s1++;
+                        AfterWinGame();
+                    }
+                    else
+                    {
+                        MessageBox.Show("O Won!",title);
+                        s2++;
+                        AfterWinGame();
+                    }
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sound2.Play();
+            this.Close();
+        }
+        void NewGame()
+        {
+            player = 2;
+            turns = 0;
+            A00.Text = A01.Text = A02.Text = A10.Text = A11.Text = A12.Text = A20.Text = A21.Text = A22.Text = "";
+            XWin.Text = "X: " + s1;
+            OWin.Text = "O: " + s2;
+            Draws.Text = "Draws: " + sd;
+            label2.Text = "PLAY NOW";
+        }
+        void AfterWinGame()
+        {
+            player = 2;
+            turns = 0;
+            A00.Text = A01.Text = A02.Text = A10.Text = A11.Text = A12.Text = A20.Text = A21.Text = A22.Text = "";
+            XWin.Text = "X: " + s1;
+            OWin.Text = "O: " + s2;
+            Draws.Text = "Draws: " + sd;
+        }
+
+        private void NGB_Click(object sender, EventArgs e)
+        {
+            label2.Text = "PLAY NOW";
+            sound2.Play(); 
+            NewGame();
+        }
+
+        bool CheckDraw()
+        {
+            if ((turns == 9)&&CheckWinner()==false)
+                return true;
+            else
+                return false;
+        }
+
+        bool CheckWinner()
+        {
+            //horizontals checks
+            if ((A00.Text == A01.Text) && (A01.Text == A02.Text) && A00.Text != "")
+                return true;
+            else if ((A10.Text == A11.Text) && (A11.Text == A12.Text) && A10.Text != "")
+                return true;
+            else if ((A20.Text == A21.Text) && (A21.Text == A22.Text) && A20.Text != "")
+                return true;
+
+            //vertical checks
+            if ((A00.Text == A10.Text) && (A10.Text == A20.Text) && A00.Text != "")
+                return true;
+            else if ((A01.Text == A11.Text) && (A11.Text == A21.Text) && A01.Text != "")
+                return true;
+            else if ((A02.Text == A12.Text) && (A12.Text == A22.Text) && A02.Text != "")
+                return true;
+
+            //diagonal checks
+            if ((A00.Text == A11.Text) && (A11.Text == A22.Text) && A00.Text != "")
+                return true;
+            else if ((A02.Text == A11.Text) && (A11.Text == A20.Text) && A02.Text != "")
+                return true;
+            else
+                return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s1 = s2 = sd = 0;
+            label2.Text = "PLAY NOW";
+            sound2.Play();
+            NewGame();
+        }
+
+        private void OWin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Draws_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void XWin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
